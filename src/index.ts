c38c6491c525bdaaffa5bf7141e12b7d73ce7778
Anida15 <anidakimtai@gmail.com>
2025-11-28 22:49:06 +0300

    Initialize TypeScript Express server with essential configurations and dependencies. Added .gitignore, package.json, package-lock.json, tsconfig.json, and basic server setup in index.ts.

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "API is running..." });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
